3d5e6bca9a9a6bc3576c1b6cdee6639eac3125e9
rindoman <igor2tak2212@gmail.com>
2024-10-05 14:44:17 +0300

    added class PlaySerializer in serializers.py

--- theater/serializers.py
+++ theater/serializers.py
@@ -23,3 +23,10 @@ class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
         fields = ("id", "first_name", "last_name")
+
+
+class PlaySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Play
+        fields = ("id", "title", "description", "genres", "actors")
