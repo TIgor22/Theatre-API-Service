55958555d807636d444f4ff4a33904f8b5edb1f5
rindoman <igor2tak2212@gmail.com>
2024-10-05 15:31:38 +0300

    created the PerformanceSerializer class in the serializer.py

--- theater/serializers.py
+++ theater/serializers.py
@@ -55,3 +55,10 @@ class TheatreHallSerializer(serializers.ModelSerializer):
     class Meta:
         model = TheatreHall
         fields = ("id", "name", "rows", "seats_in_row")
+
+
+class PerformanceSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Performance
+        fields = ("id", "play", "theatre_hall", "show_time")
