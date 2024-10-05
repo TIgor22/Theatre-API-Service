e75d2316081d33d91f7bc6fc7f135d901e597550
rindoman <igor2tak2212@gmail.com>
2024-10-05 14:35:59 +0300

    added class ActorViewSet in views.py and path in urls.py

--- theater/urls.py
+++ theater/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from theater.views import GenreViewSet
+from theater.views import (
+    GenreViewSet,
+    ActorViewSet,
+)
 
 
 router = routers.DefaultRouter()
 router.register("genres", GenreViewSet, basename="genres")
+router.register("actors", ActorViewSet, basename="actors")
 
 urlpatterns = [
     path("", include(router.urls))
--- theater/views.py
+++ theater/views.py
@@ -1,9 +1,20 @@
 from rest_framework import viewsets
 
-from theater.models import Genre
-from theater.serializers import GenreSerializer
+from theater.models import (
+    Genre,
+    Actor,
+)
+from theater.serializers import (
+    GenreSerializer,
+    ActorSerializer,
+)
 
 
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
