97dc6dd7448bc9d66e3aaea09b2d7054ba1a2549
rindoman <igor2tak2212@gmail.com>
2024-10-05 14:22:22 +0300

    added GenreViewSet to views.py

--- theater/views.py
+++ theater/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from theater.models import Genre
+from theater.serializers import GenreSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
