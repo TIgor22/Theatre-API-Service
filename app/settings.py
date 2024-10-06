c532ef8bc37a7d251b44be0e93341ef0d8af41c3
rindoman <igor2tak2212@gmail.com>
2024-10-06 16:25:49 +0300

    installed djangorestframework-simplejwt and added settings in settings.py, created paths for creating tokens and for updating them

--- app/settings.py
+++ app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "user",
     "rest_framework",
     "debug_toolbar",
+    "rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,15 @@ AUTH_USER_MODEL = "user.User"
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
+SIMPLE_JWT = {
+	"ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+	"REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+	"ROTATE_REFRESH_TOKENS": True
+}
--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 from user.views import CreateUserView
 
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="create")
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
 
 app_name = "user"
