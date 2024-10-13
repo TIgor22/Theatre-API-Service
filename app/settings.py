a5a9ee8627338aac7a38373f159ff39685eb2ad8
rindoman <igor2tak2212@gmail.com>
2024-10-13 11:04:40 +0300

    added SECRET_KEY in settings.py and create .env.sample

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-p1$76-b=*3toqwqy98aqpm)&-k)w%bjgbh!du28q*$vslr%7t3"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
